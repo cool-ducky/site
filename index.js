e7ff77ba363122e629f9c6451d47990d67c7544d
ducky <luckyduck0734@gmail.com>
2021-10-23 11:49:13 -0500

    yay

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const axios = require('axios').default
+const app = express();
+
+
+
+app.listen(500, () => console.log('App listening at http://localhost:500'));
