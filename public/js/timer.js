8d763d5de9524a32a64ac2fe75517312257f30ea
ducky <84673751+cool-ducky@users.noreply.github.com>
2021-11-02 00:03:59 +0000

    timer

---
+++ public/js/timer.js
@@ -0,0 +1,29 @@
+const countDownDate = new Date("Nov 5, 2021 15:37:25").getTime();
+
+// Update the count down every 1 second
+const timer = () => {
+
+  // Get today's date and time
+  let now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  let distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("timer").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("timer").innerHTML = "EXPIRED";
+  }
+}
+
+setInterval(timer, 1000)
