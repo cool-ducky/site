080d95f9c8ec2044707ddf10e16219fffc4470d4
ducky <84673751+cool-ducky@users.noreply.github.com>
2021-11-04 18:47:20 -0500

    yes

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const app = express();
 const bodyParser = require('body-parser');
 const path = require('path')
 
+const port = process.env.PORT || 3000
+
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -42,4 +44,4 @@ app.get('/appeals', (req, res) => {
   res.redirect("https://discord.com/api/oauth2/authorize?client_id=900535112955998271&redirect_uri=https%3A%2F%2Fimagine.cf%2Fcallback&response_type=code&scope=email%20identify")
 })
 
-app.listen(500, () => console.log('App listening at http://localhost:500'));
+app.listen(port, () => console.log('App listening at http://localhost:500'));
