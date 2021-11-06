c213cfdcc4701eda219f0890b9b65405e3f0f3cc
ducky <luckyduck0734@gmail.com>
2021-11-06 11:08:43 -0500

    oops

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const mongoose = require('mongooose')
+const mongoose = require('mongoose')
 const { verifyKeyMiddleware } = require('discord-interactions')
 require('dotenv').config()
 const axios = require('axios').default
